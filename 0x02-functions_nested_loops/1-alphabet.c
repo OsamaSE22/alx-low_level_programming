a662fa6e6c9cf9a8cd7cdfccca2f39db0c89572d
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-02-29 16:51:44 -0800

    edit betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  * main Entry point
- * 
+ * return type always (0)
  */
 void print_alphabet(void)
 {
 	char c;
-	
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar (c);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
