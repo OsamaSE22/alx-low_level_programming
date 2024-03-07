e79fed917b0979876e36fe0e375961e727f18871
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-03-06 20:48:16 -0800

    new function to print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char a ='a';
+	
+	for(i = 0; i < 10; i++)
+	{
+		for(a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
