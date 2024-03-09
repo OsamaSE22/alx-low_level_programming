afa4416c32389000c99ba3a4fc2b22d3306f2262
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-03-08 21:37:27 -0800

    this is the abs function in C i created it from scratch

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * this is to return the absoulute value
+ * of any number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
