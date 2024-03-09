091b997b9ed4c5e2a180f62bfe1553968c4442ad
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-03-08 20:55:45 -0800

    this is sign function that checks the sign of any number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * this is sign function
+ *
+ * Returns 1 and prints + if n is
+ *  greater than 0
+ *  Returns 0 and prints 0 if n is zero
+ *  Returns -1 and prints - if n
+ *   is less than zero
+ */
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
