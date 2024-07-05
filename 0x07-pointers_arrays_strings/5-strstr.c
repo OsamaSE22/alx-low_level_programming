ecfdc1fd188bb2754c21263746a5c937a5f60c52
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-07-05 09:48:11 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,12 +3,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *p;
-	for (p = haystack; *p != '\0'; p++)
+	if (*needle == '\0')
 	{
-		if (*p == *needle && *needle != '\0')
+		return haystack;
+	}
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
 		{
-			return p;
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return haystack;
 		}
 	}
 	return NULL;
