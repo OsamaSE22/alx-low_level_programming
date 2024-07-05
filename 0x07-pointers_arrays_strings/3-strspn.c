0bfa74465a5192ea71dc3adc2365dc59ed4654f5
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-07-05 06:35:44 -0700

    strpbrk.c function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -20,5 +20,5 @@ unsigned int _strspn(char *s, char *accept)
 		}
 		count++;
 	}
-	return count++;
+	return count;
 }
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p, *a;
+
+	for (p = s; *p != '\0'; p++)
+	{
+		for (a = accept; *a != '\0'; a++)
+		{
+			if(*p == *a)
+			{
+				return p;
+			}
+		}
+	}
+	return p;
+}
