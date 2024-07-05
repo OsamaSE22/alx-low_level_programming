dc9abb004b7c763f8e5569407322e936645ad686
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-07-05 09:23:27 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,7 @@ char *_strstr(char *haystack, char *needle)
 	char *p;
 	for (p = haystack; *p != '\0'; p++)
 	{
-		if (*p == needle[0])
+		if (*p == *needle && *needle != '\0')
 		{
 			return p;
 		}
