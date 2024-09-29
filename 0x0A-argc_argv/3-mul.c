9375899d841b176a3f625509f19ea7f0f730fb6c
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2024-09-29 14:44:19 -0700

    atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * main - check the code.
+
+/**
+ * main - Multiplies two numbers passed as arguments.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings representing the arguments.
  *
- * Return: Always 0.
+ * Return: 0 if successful, 1 if there is an error.
  */
 int main(int argc, char *argv[])
 {
-	if(argc > 1 && argc < 4)
-	{
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
-		int r = a * b;
-		printf("%d\n", r);
-		return (0);
-	}
-	else
-	{
-		printf("Error\n");
-		return 1;
-	}
+    int num1, num2, result;
+
+    /* Check if exactly 2 arguments are provided (excluding program name) */
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 1;
+    }
+
+    /* Convert the arguments to integers using atoi */
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    /* Multiply the two integers */
+    result = num1 * num2;
+
+    /* Print the result followed by a new line */
+    printf("%d\n", result);
+
+    return 0;
 }
+
