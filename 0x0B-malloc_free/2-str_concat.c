fe50c838174437ef451655bb109451df80958236
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-11-27 09:16:46 -0800

    ok

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,8 +5,11 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 	int i,j,a,b;
-	if (s1 == NULL || s2 == NULL)
-		return NULL;
+	 if (s1 == NULL)
+		 s1 = "";
+	 if (s2 == NULL)
+		 s2 = "";
+
 	for(a = 0; s1[a] != '\0'; a++);
 	for (b = 0; s2[b] != '\0'; b++);
 	ptr = malloc((a + b + 1)* sizeof(char));
@@ -19,5 +22,6 @@ char *str_concat(char *s1, char *s2)
 		ptr[i] = s2[j];
 		i++;
 	}
+	ptr[i] = '\0';
 	return ptr;
 }
