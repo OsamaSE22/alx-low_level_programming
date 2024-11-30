08c3d83950486d528c035f428e1fc25212be1c1a
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-11-30 14:05:21 -0800

    do

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	
 	for(i = 0; s1[i] != '\0'; i++);
 
-	ptr = malloc(sizeof(char)*(i+n));
+	ptr = malloc(sizeof(char)*(i+n+1));
 	
 
 	for(i = 0; s1[i] != '\0'; i++)
