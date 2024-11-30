95c8224f21c9ed3105b1096f741ea69c8b58a99c
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-11-30 14:01:14 -0800

    doable

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i,j;
+	unsigned int i,k,j;
+	
+	for(i = 0; s1[i] != '\0'; i++);
 
-	ptr = malloc(sizeof(char)*(strlen(s1)+n));
+	ptr = malloc(sizeof(char)*(i+n));
 	
 
 	for(i = 0; s1[i] != '\0'; i++)
@@ -21,25 +23,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			ptr[i] = s1[i];
 		}
 	}
-	if(strlen(s2) == n)
-	{
-		if(s2 == NULL)
+	for (k = 0; s2[k] != '\0'; k++);
+	if(s2 == NULL)
                 {
                         ptr[i] = ' ';
                 }
-
-		for(j = 0; j != '\0'; j++)
+	else{
+		if(k == n)
 		{
-			ptr[i] = s2[j];
-			i++;
+			for(j = 0; s2[j] != '\0'; j++)
+			{
+				ptr[i] = s2[j];
+				i++;
+			}
 		}
-	}
-	else
-	{
-		for(j = 0; j <= n; j++)
+		else
 		{
-			ptr[i] = s2[j];
-			i++;
+			for(j = 0; j <= n; j++)
+			{
+				ptr[i] = s2[j];
+				i++;
+			}
 		}
 	}
 	ptr[i] = '\0';
