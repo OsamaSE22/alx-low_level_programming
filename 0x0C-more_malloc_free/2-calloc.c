db61504851db06da807c99aa1280d6440b613342
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-12-01 12:05:38 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,7 +4,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	if(nmemb == 0 || size == 0)
+	{
+		return NULL;
+	}
 	void *ptr;
 	ptr = malloc(nmemb * size);
+	if(ptr == NULL)
+	{
+		return NULL;
+	}
 	return ptr;
 }
