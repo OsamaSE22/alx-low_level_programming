e3b51de4b4ad919d98be3a5a7647d87cdd10d15f
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-12-19 15:47:15 -0800

    dome

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include "dog.h"
 
+
+/**
+ * init_dog - Initializes a dog structure with provided values.
+ * @d: A pointer to the dog structure.
+ * @name: A string (char*) representing the dog's name.
+ * @age: A float representing the dog's age.
+ * @owner: A string (char*) representing the dog's owner's name.
+ *
+ * This function initializes the members of a dog structure with the
+ * provided values. If the provided structure pointer is NULL, the
+ * function does nothing.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d == NULL)
+	if (d == NULL)
 	{
 		return;
 	}
