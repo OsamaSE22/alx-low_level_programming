007104992d8736eb61a2833fccbd16919b67efc6
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-12-19 17:57:27 -0800

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  /* For strcpy */
 #include "dog.h"
 
 /**
@@ -14,13 +15,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *a;
 
+	/* Allocate memory for the dog structure */
 	a = malloc(sizeof(dog_t));
 	if (a == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
-	a->name = name;
+
+	/* Allocate memory for the name and owner strings and copy the content */
+	a->name = malloc(strlen(name) + 1);  /* +1 for null terminator */
+	if (a->name == NULL)
+	{
+		free(a);  /* Free previously allocated memory in case of failure */
+		return (NULL);
+	}
+	strcpy(a->name, name);
+
+	a->owner = malloc(strlen(owner) + 1);  /* +1 for null terminator */
+	if (a->owner == NULL)
+	{
+		free(a->name);  /* Free previously allocated name */
+		free(a);         /* Free the structure memory */
+		return (NULL);
+	}
+	strcpy(a->owner, owner);
+
+	/* Assign the age */
 	a->age = age;
-	a->owner = owner;
+
 	return (a);
 }
+
