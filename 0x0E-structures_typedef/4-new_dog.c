6b7e78454ca578dba709078a6f74817c78013bd7
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-12-19 17:47:36 -0800

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,6 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *a;
 
 	a = malloc(sizeof(dog_t));
+	if (a == NULL)
+	{
+		return NULL;
+	}
 	a->name = name;
 	a->age = age;
 	a->owner = owner;
