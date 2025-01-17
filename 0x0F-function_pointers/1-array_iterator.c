92ea5ce7a65674a97eca3ae84df8bdd4ca6571fa
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-17 07:23:25 -0800

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate on the array and apply the function provided
+ * provide the array, size and the function pointer pointing to the function.
+ * @array: this is array of integers.
+ * @size: this is the number of elements in the array
+ * @action: this is the function pointer that points to the funciton
+ *
+ * Rerun: void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define HEADER
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *a));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
