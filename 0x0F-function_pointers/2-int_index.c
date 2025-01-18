58ec23d9595a2b7331beea4387b2a32dca317c8b
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-17 17:23:41 -0800

    fpre

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,21 +3,20 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i,k;
 
 	if (size <= 0 || array == NULL)
-		return (-1);
+		k =-1;
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
-			cmp(array[i]);
-			return (i);
+			k = i;
+			break;
 		}
+	if(cmp(array[i]) == 0)
+		k = -1;
 	}
-	return (i);
-
-	if (cmp(array[i]) == 0)
-		return (-1);
+	return k;
 }
