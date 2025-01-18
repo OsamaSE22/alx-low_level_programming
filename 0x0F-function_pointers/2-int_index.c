01b39e4a6491f549a1bdb922e4b17f652886f962
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-17 17:55:01 -0800

    add fnuciton pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,19 +1,27 @@
 #include <stddef.h>
 #include "function_pointers.h"
 
+/**
+ * int_index - iterate through the array and apply function
+ * @array: the array pointer
+ * @size: the size of the array
+ * @cmp: the function included
+ *
+ * Return: -1 if false, something else otherwise
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array ==NULL || cmp == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
-			return i;
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
