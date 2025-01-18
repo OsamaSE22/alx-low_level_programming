79021177c04562b220e848cf3eea2fdcca3b35c0
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-17 17:03:37 -0800

    dome

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			cmp(array[i]);
+			return (i);
+		}
+	}
+	return (i);
+
+	if (cmp(array[i]) == 0)
+		return (-1);
+}
