76f27d78b63124285cc029a212e70239534bbea2
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-22 18:39:03 -0800

    done betty code

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int main(int argc, char * argv[])
+/**
+ * main - check the code
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
 	int x;
 
 	if (argc == 4)
-	{	
-			x = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-			printf("%d\n",x );
-		
+	{
+		x = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+		printf("%d\n", x);
 	}
 	else
 	{
-	printf("Error\n");
-	exit(98);
+		printf("Error\n");
+		exit(98);
 	}
 	return (0);
 }
