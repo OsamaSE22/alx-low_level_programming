3cd12a527c7282d999aee54e9d5c0a8b0172c316
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-22 18:28:00 -0800

    done

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,12 +7,10 @@ int main(int argc, char * argv[])
 	int x;
 
 	if (argc == 4)
-	{
-		if(get_op_func(argv[2]))
-		{
+	{	
 			x = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 			printf("%d\n",x );
-		}		
+		
 	}
 	else
 	{
