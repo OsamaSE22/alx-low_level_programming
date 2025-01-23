84b76d5b20bbe1c0699727b427b6f7b8b1bd8f78
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-22 18:52:54 -0800

    done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,24 +4,30 @@
 #include <string.h>
 #include "3-calc.h"
 
+/**
+ * get_op_func - check the operator
+ * @s: the operator
+ *
+ * Return: function pointer
+ */
 int (*get_op_func(char *s))(int, int)
 {
-	  op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
 	int i;
 
 	i = 0;
-	while(i < 5)
+	while (i < 5)
 	{
-		if(strcmp(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 		{
-			return ops[i].f;
+			return (ops[i].f);
 		}
 			i++;
 	}
