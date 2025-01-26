2fb72dff57bd8beab24be806e8fdfcc9752a90a7
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-01-25 16:58:39 -0800

    done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,10 +16,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int num;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	num = 0;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
