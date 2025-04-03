cd3b7ea92111574e0167bd1324ff60b358a01d26
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-04-03 11:07:35 -0700

    Done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef PRO
+#define PRO
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,8 +14,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-#ifndef PROTOTYPES
-#define PROTOTYPES
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
