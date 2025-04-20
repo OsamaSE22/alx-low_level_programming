76d225a28c4898b176fbde5ba0484a4940430826
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-04-20 10:13:32 -0700

    Done

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,6 +16,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *ptr;
 
 	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	ptr->n = n;
 	ptr->next = NULL;
 
