e27f7e99a970bf5dfd11ad8da2018daa416c967c
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-04-20 11:05:42 -0700

    Done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free the entire linked list.
+ * @head: the address of pointer pointing to the linked list.
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *current;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		current = ptr;
+		ptr = ptr->next;
+		free(current);	
+	}
+	free(ptr);
+	*head = NULL;
+}
