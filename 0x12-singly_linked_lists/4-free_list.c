eeae0b425b9910bd1d3cf24ef2f10ab267b32906
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-04-19 17:39:03 -0700

    Done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -14,11 +14,12 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
-	
-	while(head != NULL)
+
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
+
 		free(temp->str);
 		free(temp);
 	}
