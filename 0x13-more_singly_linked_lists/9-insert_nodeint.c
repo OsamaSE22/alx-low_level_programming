754105d33b01bdcadd4e7db8366c3afa4621a461
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-04-24 21:13:25 -0700

    Done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		counter = 0;
 		ptr = *head;
-		while (ptr != NULL && counter < idx)
+		while (ptr != NULL && counter < idx - 1)
 		{
 			ptr = ptr->next;
 			counter++;
