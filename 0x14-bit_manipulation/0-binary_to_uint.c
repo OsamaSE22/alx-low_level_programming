e81bddb7621bd65aabbad488358d65370e1822e1
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2025-05-05 00:34:14 -0700

    Done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+
+	int slen = strlen(b);
+	unsigned int total = 0;
+	int decval = 1;
+
+	for (i = (slen - 1); i >= 0; i--)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		if (b[i] == '1') 
+		{
+			total += decval;
+		}
+		decval *= 2;
+	}
+	return (total);
+}
