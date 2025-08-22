7191273b55b07e6a6d1cd4b329da617087136572
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-08-22 04:11:26 +0300

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - free the linked list
+ * @head: the pointer pointing to the first node
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *curr;
